d14829de52c8f32a1224fbbb31b1c9e6e571e999
Philip Watson <philip.watson@adscale.co.nz>
2020-01-14 10:36:20 +1300

    Fix stroeerCore tests
    Due to signature change of GetUsersyncInfo function and a change to a
    json schema used for tests.

--- adapters/stroeercore/usersync_test.go
+++ adapters/stroeercore/usersync_test.go
@@ -1,6 +1,8 @@
 package stroeercore
 
 import (
+	"github.com/prebid/prebid-server/privacy"
+	"github.com/prebid/prebid-server/privacy/gdpr"
 	"testing"
 	"text/template"
 
@@ -10,7 +12,12 @@ import (
 func TestStroeerCoreSyncer(t *testing.T) {
 	temp := template.Must(template.New("sync-template").Parse("http://js.lsd.test/pbsync.html?gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&redirect=http%3A%2F%2Fprebidserver.com%2Fsetuid%3Fbidder%3DstroeerCore%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D"))
 	syncer := NewStroeerCoreSyncer(temp)
-	syncInfo, err := syncer.GetUsersyncInfo("1", "ABCDEF")
+	syncInfo, err := syncer.GetUsersyncInfo(privacy.Policies{
+		GDPR: gdpr.Policy{
+			Signal:  "1",
+			Consent: "ABCDEF",
+		},
+	})
 	assert.NoError(t, err)
 	assert.Equal(t, "http://js.lsd.test/pbsync.html?gdpr=1&gdpr_consent=ABCDEF&redirect=http%3A%2F%2Fprebidserver.com%2Fsetuid%3Fbidder%3DstroeerCore%26gdpr%3D1%26gdpr_consent%3DABCDEF%26uid%3D", syncInfo.URL)
 	assert.Equal(t, "iframe", syncInfo.Type)
